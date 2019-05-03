3496824383fc8bbb1b9c4b74f30581b360fb891d
clarkrao <clarkrao25@gmail.com>
2019-05-03 22:43:57 +0800

    scheduler module

--- scheduler/dbops/api.go
+++ scheduler/dbops/api.go
@@ -1,1 +1,21 @@
 package dbops
+
+import "log"
+
+func AddVideoDeletionRecord(vid string) error {
+	insql := "INSERT INTO video_del_rec (video_id) VALUES (?)"
+	stmt, err := dbConn.Prepare(insql)
+	if err != nil {
+		log.Printf("AddVideoDeletionRecord error : ", err)
+		return err
+	}
+
+	_, err = stmt.Exec(vid)
+	if err != nil {
+		log.Printf("AddVideoDeletionRecord Exec error :", err)
+		return err
+	}
+
+	defer stmt.Close()
+	return nil
+}
--- scheduler/handler.go
+++ scheduler/handler.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"server/scheduler/dbops"
+)
+
+func vidDelRecHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	vid := p.ByName("vid-id")
+
+	if len(vid) == 0 {
+		sendResponse(w, 400, "video id should no be empty")
+		return
+	}
+
+	err := dbops.AddVideoDeletionRecord(vid)
+	if err != nil {
+		sendResponse(w, 500, "Internal server error")
+		return
+	}
+
+	sendResponse(w, 200, "")
+	return
+}
